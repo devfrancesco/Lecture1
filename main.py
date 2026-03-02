97ddf3e0e0cf743bca84fc9d8bb04f40cb299224
devfrancesco <s326406@studenti.polito.it>
2026-03-02 18:04:15 +0100

    Fine Lecture1

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ class Prodotto:
 
     @classmethod #costruisce un nuovo costruttore
     def costruttore_con_quantità_uno(cls, name: str, price: float, supplier: str):
-        cls(name, price, 1, supplier)
+        return cls(name, price, 1, supplier)
 
     @staticmethod #non sono associati a nessuna istanza
     def applica_sconto(prezzo, percentuale):
@@ -41,6 +41,47 @@ class Prodotto:
             raise ValueError("Attenzione il prezzo non può essere negativo.")
         self._price = valore
 
+    def __str__(self):
+        return f"{self.name} - disponibili {self.quantity} pezzi a {self.price}$"
+
+    def __repr__(self):
+        return f"Prodotto(name={self.name}, price= {self.price}, quantity={self.quantity}, supplier={self.supplier})"
+
+    def __eq__(self, other: object):
+
+        if not isinstance(other, Prodotto):
+            return NotImplemented
+        return (self.name == other.name
+                and self.price == other.price
+                and self.quantity == other.quantity
+                and self.supplier == other.supplier)
+
+    def __lt__(self, other:"Prodotto") -> bool :
+        return self.price < other.price
+
+    def prezzo_finale(self) -> float:
+        return self.price * (1+self.aliquota_iva)
+
+class ProdottoScontato(Prodotto):
+    def __init__(self, name: str, price: float, quantity: int, supplier: str, sconto_percento: float):
+        #Prodotto.__init__()
+        super().__init__(name, price, quantity, supplier)
+        self.sconto_percento = sconto_percento
+
+    def prezzo_finale(self) -> float:
+        return self.valore_lordo() * (1-self.sconto_percento/100)
+
+class Servizio(Prodotto):
+    def __init__(self,name: str, tariffa_oraria: float, ore: int):
+        super().__init__(name=name, price= tariffa_oraria, quantity=1, supplier=None)
+        self.ore=ore
+
+    def prezzo_finale(self) -> float:
+        return self.price * self.ore
+
+
+
+
 myproduct1 = Prodotto(name="Laptop", price=1200.0, quantity=12, supplier ="ABC")
 
 print(f"Nome prodotto: {myproduct1.name} - prezzo: {myproduct1.price}")
@@ -54,8 +95,140 @@ print(f"Nome prodotto: {myproduct2.name} - prezzo: {myproduct2.price}")
 
 print(f"Valore lordo di myproduct1: {myproduct1.valore_lordo()}")
 Prodotto.aliquota_iva = 0.24
-
 print(f"Valore lordo di myproduct1: {myproduct1.valore_lordo()}")
+
+print(myproduct1)
+
+p_a = Prodotto(name="Laptop", price=1200.0, quantity=12, supplier ="ABC")
+p_b = Prodotto("Mouse", 10, 14, "CDE")
+
+print("myproduct1 == p_a?", myproduct1 == p_a) #va a chiamare il metodo __eq__ appena implementato. Mi aspetto TRUe
+print("p_a == p_b?", p_a == p_b) #Mi aspetto FALSE
+
+mylist = [p_a, p_b, myproduct1]
+mylist.sort(reverse=True)
+
+print("lista di prodotti ordinata")
+for i in mylist:
+    print(f"- {i}")
+
+my_product_scontato = ProdottoScontato("Auricolari", 320, 1, "ABC", 10)
+my_service = Servizio("Consulenza", 100, 3)
+
+mylist.append(my_product_scontato)
+mylist.append(my_service)
+
+mylist.sort(reverse=True)
+
+for elem in mylist:
+    print(elem.name, "->" , elem.prezzo_finale() )
+
+print("-------------------------------------------------")
+# Definire una classe abbonamento che abbia come attributi: "nome, prezo_menisle, mesi". Abbonamento dovrà avere un metodo
+# per calcolare il prezzo finale, ottenuto come prezzo_mensile * mesi
+class Abbonamento:
+    def __init__(self, nome:str, prezzo_mensile: float, mesi:int):
+        self.name = nome
+        self.prezzo_mensile = prezzo_mensile
+        self.mesi = mesi
+
+    def prezzo_finale(self) -> float:
+        return self.prezzo_mensile * self.mesi
+
+abb = Abbonamento("Software gestionale", 30, 24)
+mylist.append(abb)
+for elem in mylist:
+    print(elem.name, "->" , elem.prezzo_finale() )
+
+def calcola_totale(elementi):
+    tot = 0
+    for e in elementi:
+        tot += e.prezzo_finale()
+    return tot
+
+print(f"Il totale è: {calcola_totale(mylist)}")
+
+from typing import Protocol
+
+class HaPrezzoFinale(Protocol):
+    def prezzo_finale(self) -> float:
+        ...
+
+def calcola_totale(elementi: list[HaPrezzoFinale]) -> float:
+    return sum(e.prezzo_finale() for e in elementi)
+
+print(f"Il totale è: {calcola_totale(mylist)}")
+
+#Scriviamo un codice python che modelli un semplice gestionale aziendale.
+#Dovremmo prevedere la possibilità di definire entità che modellano i prodotti,
+#i clienti, offrire interfacce per calcolare i prezzi, eventualmente scontati, ...
+print("-------------------------------------------------")
+print("Sperimentiamo con Dataclass")
+
+from dataclasses import dataclass
+
+@dataclass
+class ProdottoRecord:
+    name: str
+    prezzo_unitario: float
+
+@dataclass
+class ClienteRecord:
+    name: str
+    email: str
+    categoria: str
+
+@dataclass
+class RigaOrdine:
+    prodotto: ProdottoRecord
+    quantita: int
+
+    def totale_riga(self) -> float:
+        return self.prodotto.prezzo_unitario * self.quantita
+
+@dataclass
+class Ordine:
+    righe: list[RigaOrdine]
+    cliente: ClienteRecord
+
+    def totale_netto(self):
+        return sum(r.totale_riga() for r in self.righe)
+
+    def totale_lordo(self, aliquota_iva):
+        return self.totale_netto() * (1+aliquota_iva)
+
+    def numero_righe(self):
+        return len(self.righe)
+
+@dataclass
+class OrdineConSconto(Ordine):
+    sconto_percentuale: float
+
+    def totale_scontato(self):
+        return self.totale_lordo() * (1-self.sconto_percentuale)
+
+    def totale_netto(self):
+        netto_base = super().totale_netto()
+        return netto_base * (1- self.sconto_percentuale)
+
+cliente1 = ClienteRecord("Mario Rossi", "mariorossi@example.com", "Gold")
+p1 = ProdottoRecord("Laptop", 1200)
+p2 = ProdottoRecord("Mouse", 20)
+
+ordine = Ordine([RigaOrdine(p1,2), RigaOrdine(p2,10)], cliente1)
+ordine_scontato = OrdineConSconto([RigaOrdine(p1,2), RigaOrdine(p2,10)], cliente1, 0.1)
+
+print(ordine)
+print("Numero di righe nell'ordine: ", ordine.numero_righe())
+print("Totale netto: ", ordine.totale_netto())
+print("Totale lordo (IVA 22%): ", ordine.totale_lordo(0.22))
+
+print(ordine_scontato)
+print("Totale netto sconto: ", ordine_scontato.totale_netto())
+print("Totale lordo scontato: ", ordine_scontato.totale_lordo(0.22))
+print("-------------------------------------------------")
+
+
 
 # Scrivere una classe Cliente che abbia i campi "nome", "email", "categoria" ("Gold", "Silver", "Bronze").
 # vorremmo che questa classe avesse un metodo chiamato "descrizione"
@@ -87,6 +260,5 @@ class Cliente:
         return f"Cliente {self.name} ({self.categoria}) - {self.email}"
 
 c1 = Cliente(name="Mario Bianchi", email="mario.bianchi@polito.it", categoria="Gold")
-c2 = Cliente(name="Carlo Masone", email="dgvasb@gmail.com", categoria="Platium")
+#c2 = Cliente(name="Carlo Masone", email="dgvasb@gmail.com", categoria="Platium")
 print(c1.descrizione())
-
